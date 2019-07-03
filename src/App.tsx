9bef67ea3caa8a5097fa7ddfef609f3c36d4f163
qhen143 <hengquentin@gmail.com>
2019-07-03 21:23:15 +1200

    Completed walkthrough for base app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from 'react';
+import DropArea from './Components/DropArea/DropArea'
+import Header from './Components/Header/Header'
+import Result from './Components/Result/Result'
+
+interface IState {
+  result: string
+  filelength: number
+}
+
+class App extends React.Component<{}, IState>{
+  public constructor(props: any) {
+    super(props)
+    this.state = {
+      result: "",
+      filelength: 0,
+    }
+  }
+
+  public render() {
+    return (
+      <div>
+        <Header />
+        <DropArea setResults={this.resultstate} />
+        <Result result={this.state.result} filelength={this.state.filelength} />
+      </div>
+    );
+  }
+  public resultstate = (resultString: string, filelen: any) => {
+    this.setState({ result: resultString, filelength: filelen })
+  }
 }
 
 export default App;
